cbc2dccba6239be5d65f10da2d06cc299866bad2
5MDM <110855261+5MDM@users.noreply.github.com>
2023-11-24 16:29:53 +0000

    fixed browser bundle

--- esbuild.mjs
+++ esbuild.mjs
@@ -2,7 +2,7 @@ import * as esbuild from "esbuild";
 
 await esbuild.build({
   entryPoints: ["index.ts"],
-  bundle: true,
+  bundle: false,
   outfile: "dist/browser.js",
   target: "es6",
   platform: "browser",
